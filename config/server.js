04cd12a3dd54a29951a6030e1bb164c5145bc5ce
SSylvain1989 <sylvain.sigonnez@gmail.com>
2021-06-07 19:03:46 +0200

    feat: add structure and new posts for blog

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a370c7f427387391bb0390ca836089f7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dfc04874-b55d-4687-a853-b2e2ae0baaf3'
+};
